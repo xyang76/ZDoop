2bd3257945d3ee818f8a6b62da40fd7e9d4b66b9
xyang76 <xyang76@hawk.iit.edu>
2018-02-19 17:54:30 -0600

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# test the hadoop cluster by running wordcount
+
+# create input files 
+mkdir temp
+
+cp 1mb.txt temp/1mb.txt 
+
+# create input directory on HDFS
+hadoop fs -mkdir -p temp
+
+# put input files to HDFS
+hdfs dfs -put ./temp/* temp
+
+# run wordcount 
+hadoop jar WordCount.jar test.WordCount temp tempout
+
+# print the input files
+# echo -e "\ninput file1.txt:"
+# hdfs dfs -cat input/file1.txt
+
+# echo -e "\ninput file2.txt:"
+# hdfs dfs -cat input/file2.txt
+
+# print the output of wordcount
+# echo -e "\nwordcount output:"
+# hdfs dfs -cat output/part-r-00000
